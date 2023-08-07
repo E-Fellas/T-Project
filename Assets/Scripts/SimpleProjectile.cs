b27d4030b72e181b0ce0c79f4204b7ffc94d9611
Kauanmb77 <kauanmb@hotmail.com>
2023-08-07 15:42:38 -0300

    :sparkles: Adding SimpleProjectile Script & Object

---
+++ Assets/Scripts/SimpleProjectile.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleProjectile : MonoBehaviour
+{
+    public float speed = -2f;
+    public int damage = 10;
+
+    public PlayerVariables playerVariables;
+
+    private void Update()
+    {
+        // Move o projétil ao longo do eixo Z
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Verifica se colidiu com o jogador
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            // Causa dano ao jogador
+            if (playerVariables.GetvidaAtual() >= 0)
+            {
+                playerVariables.ReceberDano(damage);
+            }
+
+            // Destroi o projétil
+            Destroy(gameObject);
+
+            //print de debug
+            print(playerVariables.GetvidaAtual());
+        }
+    }
+}
