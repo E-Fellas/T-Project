e744e81358327057d04e7ef0bb32dc02dd31e22e
Murilo <muriloamaralrocha@outlook.com>
2023-08-11 17:49:39 -0300

    Add comments

--- Assets/Scripts/DeadZone.cs
+++ Assets/Scripts/DeadZone.cs
@@ -6,9 +6,13 @@ public class DeadZone : MonoBehaviour
 {
     private void OnCollisionEnter(Collision collision)
     {
+        // Verifica se o objeto colidido tem tag player
         if (collision.gameObject.tag == "Player")
         {
+            // Procura pelo script PlayerVariables no player
             var playerVariables = collision.gameObject.GetComponent<PlayerVariables>();
+
+            // Da dano maximo no jogador e entao o revive
             playerVariables.ReceberDano(playerVariables.vidaMaxima);
             playerVariables.PlayerRevive();
         }
