1426facbf19bb34b7c85fd86269e4daf5bb060c8
Guimeb <rm551981@fiap.com.br>
2023-08-12 00:50:00 -0300

    :sparkles: Added new enviroment and plate movement

--- Assets/Scripts/BotaodePressao.cs
+++ Assets/Scripts/BotaodePressao.cs
@@ -7,7 +7,23 @@ public class BotaodePressao : MonoBehaviour
     public GameObject SimpleProjectile;
     public bool projectileOn = false;
 
+    private Vector3 initialPlatePosition;
+    public float plateSpeed = 2.0f;
+    private Vector3 targetPlatePosition;
+
     System.Random rnd = new System.Random();
+
+    private void Start()
+    {
+        initialPlatePosition = transform.position;
+        targetPlatePosition = initialPlatePosition;
+    }
+
+    private void Update()
+    {
+        // Atualiza a posi√ß√£o da placa suavemente usando Lerp
+        transform.position = Vector3.Lerp(transform.position, targetPlatePosition, plateSpeed * Time.deltaTime);
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
@@ -16,7 +32,8 @@ public class BotaodePressao : MonoBehaviour
         {
             print("Boa sorte!!!");
 
-            projectileOn= true;
+            targetPlatePosition = initialPlatePosition - Vector3.up * 0.5f; // Desce a plaa
+            projectileOn = true;
             StartCoroutine(ProjectileRoutine());
         }
     }
@@ -29,6 +46,9 @@ public class BotaodePressao : MonoBehaviour
             ProjectileXAssis();
             yield return new WaitForSeconds(2);
         }
+
+        projectileOn = false;
+        voltaPlaca();
     }
 
     private void ProjectileXAssis()
@@ -47,15 +67,20 @@ public class BotaodePressao : MonoBehaviour
     {
         if (SimpleProjectile != null)
         {
-            // Define a posiÁ„o onde o projÈtil ser· criado
+            // Define a posi√ß√£o onde o proj√©til ser√° criado
             Vector3 spawnPosition = new Vector3(x, 4f, 9f);
 
-            // Cria o projÈtil na posiÁ„o especificada
+            // Cria o proj√©til na posi√ß√£o especificada
             Instantiate(SimpleProjectile, spawnPosition, Quaternion.identity);
         }
         else
         {
-            Debug.LogWarning("Prefab do projÈtil n„o atribuÌdo!");
+            Debug.LogWarning("Prefab do proj√©til n√£o atribu√≠do!");
         }
+    }
+
+    public void voltaPlaca()
+    {
+        targetPlatePosition = initialPlatePosition; // Volta a placa para a posi√ß√£o inicial
     }
 }
