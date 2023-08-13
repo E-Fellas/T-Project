e5f53382cf56414a44ebd45686e78c2a4f6a627a
JFA000 <jonatasfernandesandrade@gmail.com>
2023-08-13 02:01:29 -0300

    Add new message script

---
+++ Assets/Scripts/Message.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Message : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Bem vindo! Use E para interagir com objetos.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
