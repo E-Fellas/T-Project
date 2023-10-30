c16ad6afee7ebfb67e2ad10108afd15fbfbee773
Gustavo <gustavo.souzabeio@gmail.com>
2023-10-29 21:40:37 -0300

    AppleTree interactable script completed

--- Assets/Scripts/InteractionSystem/Interactions/AppleTree.cs
+++ Assets/Scripts/InteractionSystem/Interactions/AppleTree.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class AppleTree : MonoBehaviour, IInteractable
 {
     public string InteractionPrompt { get; }
+    public InventorySelector inventorySelector;
+    public Inventory_Obj maca;
 
     public bool Interact(Interactor interactor)
     {
+        inventorySelector.AddItem(maca, 1);
         Debug.Log("Colheu Maça!!");
         return true;
     }
