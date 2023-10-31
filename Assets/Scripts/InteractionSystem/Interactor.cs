00b380871614b44466fe523bfe6a5f14bea74c68
Gustavo <gustavo.souzabeio@gmail.com>
2023-10-31 09:55:53 -0300

    Add: Old Interactor script into the new one as a comment

--- Assets/Scripts/InteractionSystem/Interactor.cs
+++ Assets/Scripts/InteractionSystem/Interactor.cs
@@ -53,3 +53,39 @@ public class Interactor : MonoBehaviour
         Gizmos.DrawWireSphere(_interactionPoint.position, _interactionPointRadius);
     }
 }
+//Old interactor script
+//
+//interface IInteragir
+//{
+//    public void Interagir();
+//}
+//public class Interacoes : MonoBehaviour
+//{
+//    public Transform InteractorSource;
+//    public float InteractRange = 6f;
+//    public float SphereRadius = 0f; // You can adjust this value to change the size of the sphere
+//
+//    void Update()
+//    {
+//        Ray r = new Ray(InteractorSource.position, InteractorSource.forward);
+//
+//        // Draw the ray in green for debugging purposes
+//        Debug.DrawRay(r.origin, r.direction * InteractRange, Color.green);
+//
+//        if (Input.GetKeyDown(KeyCode.E))
+//        {
+//            //Verifica se existe algum objeto na frente do player, baseado no InteractRange
+//            if (Physics.SphereCast(r, SphereRadius, out RaycastHit hitInfo, InteractRange))
+//            {
+//                //Verifica se o objeto possui a Interface IInteragir em seu escopo
+//                if (hitInfo.collider.gameObject.TryGetComponent(out IInteragir interactObj))
+//                {
+//                    interactObj.Interagir();
+//                }
+//            }
+//
+//            Debug.DrawRay(r.origin, r.direction * InteractRange, Color.red);
+//        }
+//    }
+//
+//}
